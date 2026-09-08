d92728cb4057d8ac48b47c54901a10981772337d
Shadab Ahmed <bookings@umrahtours.co>
2026-09-08 19:40:29 +0400

    Add Travelpayouts script

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/home/Footer";
+import Script from "next/script";
 
 export default function WebsiteLayout({
   children,
@@ -13,6 +14,12 @@ export default function WebsiteLayout({
       <main>{children}</main>
 
       <Footer />
+
+      <Script
+        id="external-script"
+        strategy="afterInteractive"
+        src="https://emrldtp.com/NTcxNzMx.js?t=571731"
+      />
     </>
   );
 }
